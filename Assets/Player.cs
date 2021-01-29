b436c8d17de4f7a78a1a3f4ce0675583a28ea3f8
HOME-PC\J <jailbot134@gmail.com>
2021-01-30 01:52:09 +0200

    first commit

---
+++ Assets/Player.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms per sec")] [SerializeField] float speed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5.6f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -20f;
+    [SerializeField] float positionYawFactor = 5;
+    [SerializeField] float controllRowFactor = -20;
+
+    float xThrow, yThrow;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTransllation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+
+        float pitchDueToPositionl = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPositionl + pitchDueToControlThrow;
+        
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+
+        float roll = xThrow * controllRowFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTransllation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
